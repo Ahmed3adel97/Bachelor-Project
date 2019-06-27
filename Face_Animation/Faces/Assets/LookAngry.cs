81d6ef45be79a83bf58d3870d9f07ad3eb4a87d5
johnny1a679 <jon1a679@gmail.com>
2019-06-27 16:38:29 +0200

    super basic face animation

---
+++ Face_Animation/Faces/Assets/LookAngry.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAngry : MonoBehaviour
+{
+
+
+    private Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("Do nothing");
+            anim.Play("New State", 0, 0.25f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Debug.Log("look angry");
+            anim.Play("angry", 0, 0.25f);
+        }
+    }
+}
